ee61881d50cb50ed0942e29a3f964fbcebda787f
abduljabbarr959-sys <228962894+abduljabbarr959-sys@users.noreply.github.com>
2025-12-24 21:45:40 +0500

    Chapter_six/

---
+++ Chapter_six/alien_no_points.py
@@ -0,0 +1,5 @@
+alien_0 = {'color': 'green','speed': 'slow'}
+
+points_value = alien_0.get('points',None)
+print(points_value)
+
---
+++ Chapter_six/favourite_languages.py
@@ -0,0 +1,9 @@
+favourite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python'
+}
+
+language = favourite_languages['sarah'].title()
+print(f"Sarah favourite language is {language}")
